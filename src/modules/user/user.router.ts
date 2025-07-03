7525428df8af4c57718f1f92950c337b01a9ee8e
RafifKI <106825044+roboloc@users.noreply.github.com>
2025-07-03 20:02:15 +0700

    feat:validator & authorization (#2)

--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -1,20 +1,49 @@
 import { Router } from "express";
+import { validateBody } from "../../middlewares/validation.middleware";
+import { CreateUserDTO } from "./dto/create-user.dto";
 import { UserController } from "./user.controller";
+import { UploaderMiddleware } from "../../middlewares/uploader.middleware";
+import { JwtMiddleware } from "../../middlewares/jwt.middleware";
+import { JWT_SECRET } from "../../config/env";
 
 export class UserRouter {
   router: Router;
   userController: UserController;
+  uploaderMiddleware: UploaderMiddleware;
+  jwtMiddleware: JwtMiddleware;
 
   constructor() {
     //pada constructor ekseskusi codenya beurutan dari paling atas
     this.router = Router();
     this.userController = new UserController();
+    this.uploaderMiddleware = new UploaderMiddleware();
+    this.jwtMiddleware = new JwtMiddleware();
     this.initializeRoutes();
   }
 
   private initializeRoutes = () => {
-    this.router.get("/", this.userController.getUsers);
+    this.router.get(
+      "/",
+      this.jwtMiddleware.verifyToken(JWT_SECRET!),
+      this.jwtMiddleware.verifyRole(["ADMIN"]),
+      this.userController.getUsers
+    );
     this.router.get("/:id", this.userController.getUser);
+    this.router.post(
+      "/",
+      this.uploaderMiddleware
+        .upload()
+        .fields([{ name: "profilePic", maxCount: 1 }]),
+      this.uploaderMiddleware.fileFilter([
+        "image/jpeg",
+        "image/png",
+        "image/avif",
+        "image/heic",
+      ]), // cara menenntukan datanya tinggal control + spasi
+      validateBody(CreateUserDTO), // bisa menggunakan multiple middleware dan
+      //  dijalankan per line ini yang dinamakaan soc
+      this.userController.createUser
+    );
   };
 
   public getRouter = () => {
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,11 +1,21 @@
+import { User } from "../../generated/prisma";
 import { ApiError } from "../../utils/api-error";
+import { PasswordService } from "../auth/password.service";
+import { CloudinaryService } from "../cloudinary/cloudinary.service";
+import { MailService } from "../mail/mail.service";
 import { PrismaService } from "../prisma/prisma.service";
 
 export class UserService {
-  prisma: PrismaService;
+  private prisma: PrismaService;
+  private cloudinaryService: CloudinaryService;
+  private mailService: MailService;
+  private passwordService: PasswordService;
 
   constructor() {
     this.prisma = new PrismaService();
+    this.cloudinaryService = new CloudinaryService();
+    this.mailService = new MailService();
+    this.passwordService = new PasswordService();
   }
 
   getUsers = async () => {
@@ -13,6 +23,7 @@ export class UserService {
       omit: { password: true },
     });
   };
+
   getUser = async (id: number) => {
     const user = await this.prisma.user.findFirst({
       where: { id: id },
@@ -21,5 +32,38 @@ export class UserService {
       throw new ApiError("user not found", 400);
     }
     return user;
+  };
+
+  createUser = async (body: User, image?: Express.Multer.File) => {
+    const user = await this.prisma.user.findFirst({
+      where: { email: body.email },
+    });
+
+    if (user) {
+      throw new ApiError("email already in use!", 400);
+    }
+
+    if (image) {
+      const { secure_url } = await this.cloudinaryService.upload(image);
+      body.profilePic = secure_url;
+    }
+
+    const hashedPassword = await this.passwordService.hashPassword(
+      body.password
+    );
+
+    const newUser = await this.prisma.user.create({
+      // jika ada spread data dan ada , bagian kedua maka
+      // akan mereplace property yang sama dalam body
+      data: { ...body, password: hashedPassword },
+      omit: { password: true },
+    });
+
+    //to dan subject, lalu isinya
+    await this.mailService.sendEmail(body.email, "Selamat Datang!", "welcome", {
+      name: newUser.name,
+    });
+
+    return newUser;
   };
 }
